6937fa68768fd67e94a8e55770290b6ce97d22e6
Max2191 <maxtucker2191@gmail.com>
2021-09-07 21:03:05 +0100

    Beggining of collection component (not finished)

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './collection-preview.styles.scss'
+
+const CollectionPreview = () => (
+    <div classNeme='collection-preview'>
+    <h1 className='title'>Title</h1>
+    <div className='preview'>
+    
+    </div>
+    </div>
+
+)
+
