3d727854b19b66101af1ea29af7de7df2157de38
Max2191 <maxtucker2191@gmail.com>
2021-10-26 22:12:15 +0100

    Added CSS

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item.jsx';
 
-const CollectionPreview = ({ title, items,}) => (
-    <div classNeme='collection-preview'>
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-    {items
+      {items
         .filter((item, idx) => idx < 4)
-        .map(item => (<div key={item.id}>{item.name}</div>))}
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
-    </div>
-
+  </div>
 );
 
 export default CollectionPreview;
